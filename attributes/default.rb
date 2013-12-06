0ac051c30bc6739c097fd23c38e26b5d922cedb9
Jeff Byrnes <jeff@evertrue.com>
2013-12-06 16:28:12 -0500

    Clean up Ruby etc. as per Rubocop

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,17 +1,17 @@
-set[:exhibitor][:snapshot_dir] = "/mnt/zookeeper"
-set[:exhibitor][:transaction_dir] = "/mnt/zookeeper"
-set[:exhibitor][:log_index_dir] = "/mnt/zookeeper_log_indexes"
+set[:exhibitor][:snapshot_dir] = '/mnt/zookeeper'
+set[:exhibitor][:transaction_dir] = '/mnt/zookeeper'
+set[:exhibitor][:log_index_dir] = '/mnt/zookeeper_log_indexes'
 
 default['et_exhibitor']['defaultconfig']['zoo_cfg_extra'] = {
-  "tickTime" => "2000",
-  "initLimit" => "10",
-  "syncLimit" => "5",
-  "autopurge.snapRetainCount" => "20",
-  "autopurge.purgeInterval" => "1"
+  'tickTime' => '2000',
+  'initLimit' => '10',
+  'syncLimit' => '5',
+  'autopurge.snapRetainCount' => '20',
+  'autopurge.purgeInterval' => '1'
 }
 
 set['exhibitor']['defaultconfig']['auto_manage_instances'] = 0
 
-default['java']['jdk_version'] = "7"
+default['java']['jdk_version'] = '7'
 default[:build_essential][:compiletime] = true
-set['zookeeper']['mirror'] = "http://mirror.metrocast.net/apache/zookeeper/zookeeper-3.4.5/zookeeper-3.4.5.tar.gz"
+set['zookeeper']['mirror'] = 'http://mirror.metrocast.net/apache/zookeeper/zookeeper-3.4.5/zookeeper-3.4.5.tar.gz'
--- recipes/default.rb
+++ recipes/default.rb
@@ -9,30 +9,30 @@
 
 server_number = 2
 
-search(:node,"roles:zookeeper AND " +
+search(:node, 'roles:zookeeper AND ' +
   "chef_environment:#{node.chef_environment}").each do |n|
   unless n.name == node.name
     node.set['et_exhibitor']['defaultconfig']['zoo_cfg_extra']["server.#{server_number}"] = "#{n['ipaddress']}\:2888\:3888"
-    Chef::Log.info("ADDING SERVER: " + "#{n['ipaddress']}\:2888\:3888")
+    Chef::Log.info("ADDING SERVER: #{n['ipaddress']}\:2888\:3888")
     server_number += 1
   end
 end
 
-node.set['exhibitor']['defaultconfig']['zoo_cfg_extra'] = node['et_exhibitor']['defaultconfig']['zoo_cfg_extra'].map { |k,v|
-    "#{k}\\=#{v}"
-  }.join('&')
+node.set['exhibitor']['defaultconfig']['zoo_cfg_extra'] = node['et_exhibitor']['defaultconfig']['zoo_cfg_extra'].map { |k, v|
+  "#{k}\\=#{v}"
+}.join('&')
 
-include_recipe "build-essential"
+include_recipe 'build-essential'
 
-node.override['java']['jdk_version'] = "7"
+node.override['java']['jdk_version'] = '7'
 
-include_recipe "zookeeper"
+include_recipe 'zookeeper'
 
-link "/etc/init.d/exhibitor" do
-  to "/lib/init/upstart-job"
+link '/etc/init.d/exhibitor' do
+  to '/lib/init/upstart-job'
 end
 
-service "exhibitor" do
-  supports :status => true, :restart => true
-  action [ :enable, :start ]
+service 'exhibitor' do
+  supports status: true, restart: true
+  action [:enable, :start]
 end
