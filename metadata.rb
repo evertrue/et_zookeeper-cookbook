e527dc785fea676d420f4841adc86404c1228ede
Eric Herot <eric.github@herot.com>
2013-12-06 15:30:46 -0500

    Attempt to build a cluster with chef

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,9 @@ maintainer_email 'eric.herot@evertrue.com'
 license          'All rights reserved'
 description      'Installs/Configures et_zookeeper'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '1.0.1'
 
 depends 'apt'
 depends 'build-essential'
-depends 'zookeeper', '~> 1.4.6'
+depends 'zookeeper'
+depends 'java'
