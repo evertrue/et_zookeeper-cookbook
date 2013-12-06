e48d898aa8426ae6bcb73605d9e974e437ad7342
Jeff Byrnes <jeff@evertrue.com>
2013-12-06 17:07:55 -0500

    Ensure `myid` file is created to go along w/ cluster config changes
    See http://zookeeper.apache.org/doc/current/zookeeperAdmin.html#sc_zkMulitServerSetup for details.

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,11 +9,26 @@
 
 server_number = 2
 
+# Build a list of Zookeeper nodes via Chef Search to use in zoo.cfg
+# see zookeeper.apache.org/doc/current/zookeeperAdmin.html#sc_zkMulitServerSetup
+# for details on why this is necessary
 search(:node, 'roles:zookeeper AND ' +
   "chef_environment:#{node.chef_environment}").each do |n|
   unless n.name == node.name
     node.set['et_exhibitor']['defaultconfig']['zoo_cfg_extra']["server.#{server_number}"] = "#{n['ipaddress']}\:2888\:3888"
     Chef::Log.info("ADDING SERVER: #{n['ipaddress']}\:2888\:3888")
+
+    # Seems that the `:snapshot_dir` is what's used for the ZK data dir
+    # See github.com/SimpleFinance/chef-zookeeper/blob/master/templates/default/defaultconfig.exhibitor.erb#L1
+    # The `content` has to be a string, thus the interpolation
+    file "#{node[:exhibitor][:snapshot_dir]}/myid" do
+      action :create
+      backup false
+      content "#{server_number}"
+      mode 644
+      owner node[:zookeeper][:user]
+    end
+
     server_number += 1
   end
 end
