113c2d3b98326a10a327e110915c2d7e8471fce4
Jeff Byrnes <jeff@evertrue.com>
2014-05-02 12:16:42 -0400

    Fix FC019: Access node attributes in a consistent manner
    Use either symbols or strings, but not both

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,17 +1,17 @@
-set[:exhibitor][:snapshot_dir] = '/mnt/zookeeper'
-set[:exhibitor][:transaction_dir] = '/mnt/zookeeper'
-set[:exhibitor][:log_index_dir] = '/mnt/zookeeper_log_indexes'
+set['exhibitor']['snapshot_dir'] = '/mnt/zookeeper'
+set['exhibitor']['transaction_dir'] = '/mnt/zookeeper'
+set['exhibitor']['log_index_dir'] = '/mnt/zookeeper_log_indexes'
 
 # Use S3 for Exhibitor's shared configuration
 
 # Load encrypted credentials
 aws_creds = Chef::EncryptedDataBagItem.load('secrets', 'aws_credentials')['ZookeeperS3']
 
-set[:exhibitor][:opts][:configtype] = 's3'
-set[:exhibitor][:s3key] = aws_creds['access_key_id']
-set[:exhibitor][:s3secret] = aws_creds['secret_access_key']
-set[:exhibitor][:opts][:s3region] = 'us-east-1'
-set[:exhibitor][:opts][:s3config] = "ops.evertrue.com:zookeeper-#{node.chef_environment}"
+set['exhibitor']['opts']['configtype'] = 's3'
+set['exhibitor']['s3key'] = aws_creds['access_key_id']
+set['exhibitor']['s3secret'] = aws_creds['secret_access_key']
+set['exhibitor']['opts']['s3region'] = 'us-east-1'
+set['exhibitor']['opts']['s3config'] = "ops.evertrue.com:zookeeper-#{node.chef_environment}"
 
 default['et_exhibitor']['defaultconfig']['zoo_cfg_extra'] = {
   'tickTime' => '2000',
@@ -24,5 +24,5 @@ default['et_exhibitor']['defaultconfig']['zoo_cfg_extra'] = {
 set['exhibitor']['defaultconfig']['auto_manage_instances'] = 1
 
 default['java']['jdk_version'] = '7'
-default[:build_essential][:compiletime] = true
+default['build_essential']['compiletime'] = true
 set['zookeeper']['mirror'] = 'http://mirror.metrocast.net/apache/zookeeper/zookeeper-3.4.5/zookeeper-3.4.5.tar.gz'
