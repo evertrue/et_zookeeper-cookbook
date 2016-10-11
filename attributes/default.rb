152437258b745ef416d32969cfcb077dad3c5f78
Eric Herot <eric.github@herot.com>
2016-10-11 12:09:35 -0400

    Drop exhibitor cookbook (and associated attributes) and use ZK cookbook directly

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,18 +1,8 @@
 default['zookeeper']['version']     = '3.4.6'
-default['zookeeper']['checksum']    =
-  '01b3938547cd620dc4c93efe07c0360411f4a66962a70500b163b59014046994'
-override['zookeeper']['service_style'] = 'exhibitor'
 
-override['zookeeper']['config']['dataDir'] = '/mnt/zookeeper'
-override['zookeeper']['config']['initLimit'] = '11'
-override['zookeeper']['config']['syncLimit'] = '17'
-override['zookeeper']['config']['autopurge.snapRetainCount'] = '20'
-override['zookeeper']['config']['autopurge.purgeInterval'] = '1'
-
-override['zookeeper']['env_vars'] = {
-  'ZOO_LOG4J_PROP' => 'INFO,ROLLINGFILE',
-  'ZOO_LOG_DIR' => node['zookeeper']['config']['dataDir']
-}
+default['et_zookeeper']['home_dir'] = '/mnt/zookeeper'
+default['et_zookeeper']['data_dir'] = "#{node['et_zookeeper']['home_dir']}/data"
+default['et_zookeeper']['log_dir']  = "#{node['et_zookeeper']['home_dir']}/logs"
 
 if node.chef_environment == 'prod'
   default['et_monitoring']['datadog']['zookeeper'] = {
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,17 +7,25 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# Set credentials for S3 access
-aws_creds = data_bag_item('secrets', 'aws_credentials')['ZookeeperS3']
-node.override['exhibitor']['s3']['access-key-id'] = aws_creds['access_key_id']
-node.override['exhibitor']['s3']['access-secret-key'] = aws_creds['secret_access_key']
+zookeeper node['zookeeper']['version'] do
+  user_home node['et_zookeeper']['home_dir']
+  data_dir node['et_zookeeper']['data_dir']
+end
 
-node.override['exhibitor']['config']['zoo-cfg-extra'] =
-  node['et_exhibitor']['defaultconfig']['zoo-cfg-extra']
-  .map { |k, v| "#{k}\\=#{v}" }.join '&'
+zookeeper_config 'zoo.cfg' do
+  config(
+    'dataDir' => node['et_zookeeper']['data_dir'],
+    'clientPort' => 2181,
+    'initLimit' => 11,
+    'syncLimit' => 17,
+    'autopurge.snapRetainCount' => 20,
+    'autopurge.purgeInterval' => 1
+  )
+  env_vars('ZOO_LOG4J_PROP' => 'INFO,ROLLINGFILE')
+  log_dir node['et_zookeeper']['log_dir']
+end
 
-include_recipe 'exhibitor'
-include_recipe 'exhibitor::service'
+zookeeper_service 'zookeeper'
 
 ohai_plugin 'et_zookeeper'
 ohai_plugin 'myid'
