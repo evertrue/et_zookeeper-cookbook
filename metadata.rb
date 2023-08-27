2d330651131bc6bd871fbba766a4203868586ded
Roderick Day <80702348+rday-evertrue@users.noreply.github.com>
2023-08-27 19:03:19 -0400

    Upgrade to zookeeper v14.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'devops@evertrue.com'
 license          'All rights reserved'
 description      'Installs/Configures et_zookeeper'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '5.0.7'
+version          '6.0.0'
 
-depends 'zookeeper', '~> 8.0'
+depends 'zookeeper'
 depends 'storage'
 depends 'filebeat'
 depends 'ohai', '>= 4.0'
